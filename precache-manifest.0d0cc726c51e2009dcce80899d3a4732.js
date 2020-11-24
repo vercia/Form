d9f2da134f029c7761d8a1d877ad2d035a0e7f17
Weronika <werawera112@gmail.com>
2020-11-24 22:01:37 +0100

    Updates

---
+++ precache-manifest.0d0cc726c51e2009dcce80899d3a4732.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5630fd8be619b33e3d0a2f1e7523850",
+    "url": "/Form/index.html"
+  },
+  {
+    "revision": "ca5132f8647541b7e6d4",
+    "url": "/Form/static/css/main.8337b5b0.chunk.css"
+  },
+  {
+    "revision": "000c594e3aad4842e641",
+    "url": "/Form/static/js/2.23ebce42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Form/static/js/2.23ebce42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca5132f8647541b7e6d4",
+    "url": "/Form/static/js/main.c2cd4f06.chunk.js"
+  },
+  {
+    "revision": "9b1ab879a6fcf1058293",
+    "url": "/Form/static/js/runtime-main.7b89d411.js"
+  }
+]);
